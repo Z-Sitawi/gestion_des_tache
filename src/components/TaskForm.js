66d5667767c1814f4c82179d220381bb16c2013e
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-11-19 17:55:33 +0100

    create TaskForm component

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function TaskForm() {
+export default function TaskForm(props) {
+  const [show, setSow] = useState(props.show);
+  const cancel = () => {
+    setSow(false);
+  };
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      {show && (
+        <div className={(props.mainBtnTxt === "Modifier")? "bg-success p-2 card col-6": "bg-primary p-2 card col-6"}>
+          <p className="display-5 text-center">
+            <b>{props.title}</b>
+          </p>
+          <div className="bg-light p-2 d-flex gap-3 flex-column align-items-center">
+            <textarea
+              className="col-12"
+              rows={3}
+              placeholder="Ecrire votre tâche ici"
+              type="text"
+            />
+            <div>
+              <button className={(props.mainBtnTxt === "Modifier")? "btn btn-success m-1" : "btn btn-primary m-1"}>
+                {props.mainBtnTxt}
+              </button>
+              <button onClick={cancel} className="btn btn-danger m-1">
+                Annuler
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
